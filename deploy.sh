9f5c430b3ad0ccf932da937942d2636ad758d300
Denis Shevchenko <me@dshevchenko.biz>
2015-01-11 21:44:45 +0400

    Новый блог.

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-# Скрипт для для автоматического обновления блога на gh-pages. 
+# Скрипт для для автоматического обновления сайта на GitHub Pages. 
 
-USAGE="Запускаем так: ./deploy.sh \"Сообщение о коммите\" 
+USAGE="
+Запускаем так: ./deploy.sh \"Сообщение о коммите\"
 
 Пример:
   ./deploy.sh \"Обновление стиля.\"
@@ -11,40 +12,37 @@ USAGE="Запускаем так: ./deploy.sh \"Сообщение о комми
 # При любой ошибке скрипт вылетает...
 set -e
 
-# Проверяем наличие коммит-сообщения...  
-if [ "$1" = "" ]
-then
-    echo "А сообщение о коммите где?" 
-    echo "$USAGE" 
-    exit 1
-fi
-
 # Устанавливаем переменную, для нашего коммит-сообщения...  
 COMMIT_MESSAGE=$1
 
-echo "Заливаем в мастер..."
+if [ "$1" = "" ]
+then
+    echo "А сообщение о коммите где?"
+    echo "${USAGE}"
+    exit 1
+fi
+
+echo "Учитываем изменения в ветке master..."
+git add .
 git commit -a -m "$COMMIT_MESSAGE"
 git push -f origin master
 
 echo "Собираем новую версию сайта..."
-ghc site.hs
-./site rebuild
+./just_build.sh
 
 echo "Копируем во временное место, предварительно удалив старое, если нужно..."
 rm -rf /tmp/_site/ || true 1> /dev/null
 cp -R _site /tmp/
 
-echo "Переключаемся на gh-pages..."
+echo "Переключаемся на ветку 'gh-pages'..."
 git checkout gh-pages
 
 echo "Копируем прямо в корень содержимое подготовленного каталога _site..."
 cp -R /tmp/_site/* .
 
-echo "Учитываем все последние новшества..."
+echo "Учитываем все последние новшества, если таковые имеются, и публикуем на GitHub Pages..."
 git add .
 git commit -a -m "$COMMIT_MESSAGE"
-
-echo "Заливаем на GitHub Pages..."
 git push -f origin gh-pages
 
 echo "Возвращаемся в мастер..."
